13aa0a2fe4179d156cc447db7a393649bf2adb1a
FroZo <frozo02@yandex.ru>
2020-08-01 11:49:12 +0800

    Rewrite cron scrap task

--- src/cron/tasks/scrap.js
+++ src/cron/tasks/scrap.js
@@ -1,225 +1,216 @@
-"use strict";
-
+// Libs
 const fetch = require( "node-fetch" );
-const { getPool } = require( "../../database/pool" );
-const Transaction = require( "../../database/transaction" );
-const users = require( "../../database/users" );
+
+// Heleprs
 const { getManyMediaUrl, getOneMediaUrl } = require( "../../helpers/urlFactory" );
 const sign = require( "../../helpers/sign" );
 
-module.exports = {
-  run
-};
+// Database
+const { getPool } = require( "../../database/pool" );
+const Transaction = require( "../../database/transaction" );
+const users = require( "../../database/users" );
+const tempTable = require( "../../database/tempTable" );
 
-async function getAllMedia( accessToken ){
+module.exports = run;
+
+/*
+ * Функция, инициализируя которую можно получать
+ * данные из аккаунта инстаграма частями.
+ * Возвращает JSON из двух полей:
+ * * fetch:
+ * *    асинхронная функция, возвращающая Promise,
+ * *    который зарезолвится либо с ошибкой, либо
+ * *    с данными
+ * * hasNextPart:
+ * *    функция, возвращающая true/false
+ * *    в зависимости от того, можно ли
+ * *    получить что-то ещё, или нет
+ */
+function initMediaFetcher( accessToken ){
   let next = getManyMediaUrl( accessToken );
-  const media = [];
 
-  while( next !== undefined ) try{
-    const response = await fetch( next );
-    const { error, data: media_, paging } = await response.json();
+  return ( {
+    fetch: async () => {
+      const media = [];
+      let response;
 
-    if( error ) return { error: {
-      ...error,
-      custom: false
-    } };
+      try{
+        console.log( "[TASK SCRAP] Get part of media" );
+        response = await fetch( next );
 
-    if( typeof paging === "object" && "next" in paging ) next = paging.next;
-    else next = undefined;
+        if( !response.ok ){
+          console.debug( response );
 
-    if( Array.isArray( media_ ) ) for( const el of media_ ){
-      const { id, children } = el;
+          return { error: { custom: true } };
+        }
+      } catch( e ) {
+        console.error( e );
 
-      media.push( el );
-
-      if( children ) for( const { id: childrenId } of children.data ){
-        const response = await fetch( getOneMediaUrl( childrenId, accessToken, "id,media_type,media_url,permalink,thumbnail_url,timestamp,username" ) );
-        const result = await response.json();
-
-        media.push( {
-          ...result,
-          parent_id: id
-        } );
+        return { error: { custom: true } };
       }
-    }
-  } catch( e ) {
-    console.log( "[TASK SCRAP] INVALID REQUEST TO INSTAGRAM" );
-    console.log( e );
 
-    return { error: { custom: true } };
-  }
+      const { error, data: media_, paging } = await response.json();
 
-  return { media };
-}
+      if( error ){
+        console.debug( error );
 
-async function filter( client, oracleUserId, media, updatedAt ){
-  const mediaIds = media.map( ( { id } ) => id );
-  let { rows: mediaIds_ } = await client.query(
-    `select caption, id
-    from user_media
-    where
-      oracle_user_id = $1 and
-      ( status != 'deleted' or status is null )`,
-    [ oracleUserId ]
-  );
+        return { error: {
+          ...error,
+          custom: false
+        } };
+      }
 
-  mediaIds_ = mediaIds_.map( ( { id } ) => id );
+      if( typeof paging === "object" && "next" in paging ) next = paging.next;
+      else next = undefined;
 
-  // const q = media
-  //   .map( ( { caption, timestamp } ) => [
-  //     caption,
-  //     ( new Date( timestamp ) ).toString(),
-  //     Math.floor( ( new Date( timestamp ) ).getTime() / 1000 )
-  //   ] )
-  //   .sort( ( { timestamp: at }, { timestamp: bt } ) => {
-  //     if( ( new Date( at ) ) < ( new Date( bt ) ) ) return 1;
-  //     else if( ( new Date( at ) ) > ( new Date( bt ) ) ) return -1;
-  //
-  //     return 0;
-  //   } );
-  //
-  // console.log( q );
+      if( Array.isArray( media_ ) ) for( const el of media_ ){
+        const { id, children } = el;
 
-  const created = media.filter( ( { id, timestamp } ) =>
-    !mediaIds_.includes( id ) &&
-    Math.floor( ( new Date( timestamp ) ).getTime() / 1000 ) > updatedAt
-  );
-  const deleted = mediaIds_.filter( id => !mediaIds.includes( id ) );
+        media.push( el );
 
-  return { created, deleted };
-}
+        if( children ) for( const { id: childrenId } of children.data ){
+          try{
+            console.log( "[TASK SCRAP] Get one media" );
+            response = await fetch( getOneMediaUrl( childrenId, accessToken, "id,media_type,media_url,permalink,thumbnail_url,timestamp,username" ) );
 
-function save( client, oracleUserId, media, createdAt ){
-  const fields = [
-    "oracle_user_id",
-    "caption",
-    "id",
-    "media_type",
-    "media_url",
-    "permalink",
-    "thumbnail_url",
-    "timestamp",
-    "username",
-    "parent_id",
-    "created_at"
-  ];
+            if( !response.ok ){
+              console.debug( response );
 
-  const values = media.map( ( {
-    caption,
-    thumbnail_url,
-    parent_id,
-    id,
-    media_type,
-    media_url,
-    permalink,
-    timestamp,
-    username
-  } ) => {
-    caption = caption ? `'${caption}'` : "null";
-    thumbnail_url = thumbnail_url ? `'${thumbnail_url}'` : "null";
-    parent_id = parent_id ? `'${parent_id}'` : "null";
+              // continue;
+              return { error: { custom: true } };
+            }
+          } catch( e ) {
+            console.error( e );
 
-    return "(" +
-      `${oracleUserId},` +
-      `${caption},` +
-      `'${id}',` +
-      `'${media_type}',` +
-      `'${media_url}',` +
-      `'${permalink}',` +
-      `${thumbnail_url},` +
-      `'${timestamp}',` +
-      `'${username}',` +
-      `${parent_id},` +
-      createdAt +
-    ")";
+            // continue;
+            return { error: { custom: true } };
+          }
+
+          const result = await response.json();
+
+          media.push( {
+            ...result,
+            parent_id: id
+          } );
+        }
+      }
+      else return { media: [] };
+
+      return { media };
+    },
+    hasNextPart: () => next !== undefined
   } );
-
-  return client.query(
-    `insert into user_media( ${fields} )
-    values ${values}`
-  );
 }
 
-function del( client, oracleUserId, ids ){
-  return client.query(
-    `update user_media
-    set status = 'deleted'
-    where
-      oracle_user_id = $1 and
-      id = any( $2 )`,
-    [ oracleUserId, ids ]
-  );
-}
-
+/*
+ * Идея -- получать данные пачками, сохранять
+ * во временную таблицу, затем выявлять созданные записи
+ * и записывать их в основную таблицу, удалять записи из
+ * основной таблицы, которые были в аккаунте, но были
+ * удалены
+ */
 async function run( { oracleUserId }, cron ){
+  console.log( "[TASK SCRAP] Start" );
+
   const pool = getPool();
-  const { expires_at, access_token, updated_at } = await users.getByOracleUserId( pool, oracleUserId );
+  let result;
 
-  console.log( "[TASK SCRAP] START" );
-
-  const { error, media } = await getAllMedia( access_token );
-
-  if( error ){
-    console.log( `[TASK SCRAP] ERROR (${error.code}, ${error.custom})` );
-
-    if( !error.custom && ( error.code === 100 || error.code === 190 ) ){
-      console.log( "[TASK SCRAP] REMOVE USER, DELETE TASK && SEND REQUEST FOR UNBIND" );
-
-      const {
-        ORACLE_ID,
-        HMAC_ALGORITHM: algorithm,
-        HMAC_SECRET: secret,
-        HMAC_SIGNATURE_ENCODING: encoding
-      } = process.env;
-
-      const body = JSON.stringify( { oracle_user_id: `${oracleUserId}` } );
-      const signature = sign( body, algorithm, secret, encoding );
-      const taskId = await users.removeOneByOracleUserId( pool, oracleUserId );
-
-      await cron.delete( taskId );
-      console.log( `[TASK SCRAP] DATA FOR REQUEST TO ODS. SIGNATURE: ${signature}, BODY: ${body}` );
-
-      const response = await fetch( `https://dev.oracle.iterra.world/api/v1/oracle/${ORACLE_ID}/unbind`, {
-        method: "POST",
-        headers: {
-          "Accept": "application/json",
-          "Content-Type": "application/json",
-          "Signature": signature
-        },
-        body
-      } );
-
-      console.log( "[TASK SCRAP] RESPONSE FROM ODS" );
-      console.log( response );
-
-      if( !response.ok ){
-        console.log( "[TASK SCRAP] ERROR" );
-
-        return false;
-      }
-
-      console.log( "[TASK SCRAP] SUCCESS" );
-
-      return true;
-    }
+  try{
+    console.log( `[TASK SCRAP] Get user` );
+    result = await users.get( pool, oracleUserId );
+    console.log( `[TASK SCRAP] Clear temp table by oracle user id (${oracleUserId})` );
+    await tempTable.clearByOracleUserId( pool, oracleUserId );
+  } catch( e ) {
+    console.error( e );
 
     return false;
   }
 
-  const client = new Transaction( pool );
+  const { expires_at, access_token, updated_at, task_id } = result;
+  const mediaFetcher = initMediaFetcher( access_token );
 
-  if( media.length > 0 ){
-    const { created, deleted } = await filter( client, oracleUserId, media, updated_at );
+  while( mediaFetcher.hasNextPart() ){
+    const { error, media } = await mediaFetcher.fetch();
 
-    console.log( `[TASK SCRAP] CREATED: ${created.length}` );
-    console.log( `[TASK SCRAP] DELETED: ${deleted.length}` );
+    if( error ){
+      if( !error.custom && ( error.code === 100 || error.code === 190 ) ){
+        try{
+          const {
+            HMAC_ALGORITHM,
+            HMAC_SECRET,
+            HMAC_SIGNATURE_ENCODING,
+            ODS_URL,
+            UNBIND_URI,
+            ORACLE_ID
+          } = process.env;
 
-    if( created.length > 0 ) await save( client, oracleUserId, created, Math.floor( Date.now() / 1000 ) );
-    if( deleted.length > 0 ) await del( client, oracleUserId, deleted );
+          const body = JSON.stringify( { oracle_user_id: `${oracleUserId}` } );
+          const signature = sign( body, HMAC_ALGORITHM, HMAC_SECRET, HMAC_SIGNATURE_ENCODING );
+
+          console.debug( `[TASK SCRAP] Data for request. Signature: ${signature}, body: ${body}` );
+          console.log( "[TASK SCRAP] Send request for unbind" );
+
+          const response = await fetch( `${ODS_URL}${UNBIND_URI.replace( "{ORACLE_ID}", ORACLE_ID )}`, {
+            method: "DELETE",
+            headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+              "Signature": signature
+            },
+            body
+          } );
+          // #fix
+          // const response = { ok: true };
+
+          if( !response.ok ){
+            console.debug( response );
+
+            return false;
+          }
+        } catch( e ) {
+          console.error( e );
+
+          return false;
+        }
+
+        try{
+          console.log( `[TASK SCRAP] Delete from cron` );
+          await cron.delete( task_id );
+
+          return true;
+        } catch( e ) {
+          console.error( e );
+
+          return false;
+        }
+      }
+
+      return false;
+    }
+
+    try{
+      console.log( `[TASK SCRAP] Add part of media to temp table (${media.length})` );
+      await tempTable.create( pool, oracleUserId, media, Math.floor( Date.now() / 1000 ) );
+    } catch( e ) {
+      console.error( e );
+
+      return false;
+    }
   }
 
-  await users.editByOracleUserId( client, oracleUserId, { updated_at: Math.floor( Date.now() / 1000 ) } );
-  await client.end();
+  try{
+    const client = new Transaction( pool );
 
-  console.log( "[TASK SCRAP] USER LAST UPDATED TIME *UPDATED*" );
+    console.log( "[TASK SCRAP] Merge with main table" );
+    await tempTable.merge( client, oracleUserId, updated_at );
+    console.log( `[TASK SCRAP] Clear temp table by oracle user id` );
+    await tempTable.clearByOracleUserId( client, oracleUserId );
+    console.log( "[TASK SCRAP] Update user last updated time" );
+    await users.edit( client, oracleUserId, { updated_at: Math.floor( Date.now() / 1000 ) } );
+    await client.end();
+  } catch( e ) {
+    console.error( e );
+
+    return false;
+  }
 }
